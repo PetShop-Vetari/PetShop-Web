fe6bf8b7abef6a70fca2df1262237236e155de2d
João Bagatoli <joaovitorbagatoli07@gmail.com>
2024-10-24 10:24:00 -0300

    Adicionar arquivos de projeto.

---
+++ PetShop.Domain/Entities/Pet.cs
@@ -0,0 +1,9 @@
+﻿namespace PetShop.Domain.Entities;
+
+public class Pet
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public double Weight { get; set; }
+    public string? Color { get; set; }
+}
---
+++ PetShop.Infrastructure/Persistence/PetShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Domain.Entities;
+
+namespace PetShop.Infrastructure.Persistence;
+
+public class PetShopDbContext : DbContext
+{
+    internal DbSet<Pet> Pets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("string de conexão banco");
+    }
+}
